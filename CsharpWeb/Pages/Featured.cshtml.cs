77838857f315f861cacf72827f218a99683f0a03
aleiadevore <2496@holbertonschool.com>
2021-08-27 15:04:40 -0500

    Initial commit for new repo

---
+++ CsharpWeb/Pages/Featured.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CsharpWeb.Pages
+{
+    public class FeaturedModel : PageModel
+    {
+        private readonly ILogger<FeaturedModel> _logger;
+
+        public FeaturedModel(ILogger<FeaturedModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
